50b66805a8bd243c44f1c9caa77e6bab65aecde3
csp <csp.austin.sumit@gmail.com>
2018-06-08 14:23:08 -0700

    test

--- sumit/index.php
+++ sumit/index.php
@@ -18,7 +18,7 @@
          $arg1 = test_input($_POST["arg1"]);
          $arg2 = test_input($_POST["arg2"]);
          $arg3 = test_input($_POST["arg3"]);
-         exec("/usr/lib/cgi-bin/student4/calc " . $arg1 . " " . $arg2, $output, $retc); 
+         exec("/usr/lib/cgi-bin/student4/calc " . $arg1 . " " . $arg2 . " " . $arg3, $output, $retc); 
        }
 
        function test_input($data) {
